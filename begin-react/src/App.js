86b8ca153cf94f4da3e45ab954590acc20719a91
Song's house <zken14@gmail.com>
2023-06-12 21:50:52 +0900

    begin-react 230612

--- begin-react/src/App.js
+++ begin-react/src/App.js
@@ -1,13 +1,11 @@
 import React from "react";
-import Hello from "./Hello";
-import Wrapper from "./Wrapper";
+import InputSample from "./InputSample";
 
 function App() {
   return (
-    <Wrapper>
-      <Hello name="react" color="red" isSpecial />
-      <Hello color="pink" />
-    </Wrapper>
+    <>
+      <InputSample />
+    </>
   );
 }
 
