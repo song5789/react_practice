2a4df31c6c1f5e8f144c8d5ef7ae92485ef9570a
Song's house <zken14@gmail.com>
2023-06-21 23:03:07 +0900

    at home 230621

--- begin-react/src/App.js
+++ begin-react/src/App.js
@@ -1,7 +1,7 @@
-import React, { useCallback, useMemo, useRef, useState, useReducer } from "react";
+import React, { useMemo, useReducer } from "react";
 import UserList from "./UserList";
 import CreateUser from "./CreateUser";
-import useInputs from "./hooks/useInputs";
+import { produce } from "../node_modules/immer/dist/immer";
 
 function countActiveUsers(users) {
   console.log("활성 사용자 수를 세는중...");
@@ -38,20 +38,19 @@ const initialState = {
 function reducer(state, action) {
   switch (action.type) {
     case "CREATE_USER":
-      return {
-        inputs: initialState.inputs,
-        users: state.users.concat(action.user),
-      };
+      return produce(state, (draft) => {
+        draft.users.push(action.user);
+      });
     case "TOGGLE_USER":
-      return {
-        ...state,
-        users: state.users.map((user) => (user.id === action.id ? { ...user, active: !user.active } : user)),
-      };
+      return produce(state, (draft) => {
+        const user = draft.users.find((user) => user.id === action.id);
+        user.active = !user.active;
+      });
     case "REMOVE_USER":
-      return {
-        ...state,
-        users: state.users.filter((user) => user.id !== action.id),
-      };
+      return produce(state, (draft) => {
+        const index = draft.users.findIndex((user) => user.id === action.id);
+        draft.users.splice(index, 1);
+      });
     default:
       return state;
   }
