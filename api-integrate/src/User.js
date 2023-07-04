862951a6a3d75bfc01b9e3f377ea83ca876cdc09
Song's house <zken14@gmail.com>
2023-07-04 23:46:31 +0900

    at home 230704

---
+++ api-integrate/src/User.js
@@ -0,0 +1,43 @@
+import React from "react";
+import axios from "../node_modules/axios/index";
+import useAsync from "./useAsync";
+
+async function getUsers(id) {
+  const response = await axios.get(`https://jsonplaceholder.typicode.com/users/${id}`);
+  return response.data;
+}
+
+function User({ id }) {
+  const [state] = useAsync(() => getUsers(id), [id]);
+  const { loading, data: user, error } = state;
+
+  if (loading) return <div>로딩중..</div>;
+  if (error) return <div>에러가 발생했습니다.</div>;
+  if (!user) return null;
+
+  return (
+    <div style={{ border: "1px solid black", width: 600, margin: "1rem", padding: "0.5rem" }}>
+      <h2>{user.username}</h2>
+      <p>
+        <b>Email:</b> {user.email}
+      </p>
+      <p>
+        <b>Website:</b> {user.website}
+      </p>
+      <div style={{ borderTop: "1px solid black", borderBottom: "1px solid black" }}>
+        <p>
+          <b>Company:</b> {user.company.name} <br />
+          <b>Catch Phrase:</b> {user.company.catchPhrase}
+          <br />
+          <b>{user.phone}</b>
+        </p>
+      </div>
+      <p>
+        <b>Tel: </b>
+        {user.phone}
+      </p>
+    </div>
+  );
+}
+
+export default User;
--- api-integrate/src/Users.js
+++ api-integrate/src/Users.js
@@ -1,44 +1,33 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useReducer } from "react";
 import axios from "../node_modules/axios/index";
+import useAsync from "./useAsync";
+import User from "./User";
+
+async function getUsers() {
+  const reponse = await axios.get("https://jsonplaceholder.typicode.com/users");
+  return reponse.data;
+}
 
 function Users() {
-  const [users, setUsers] = useState(null);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(null);
+  const [userId, setUserId] = useState(null);
+  const [state, refetch] = useAsync(getUsers, [], true);
+  const { loading, data: users, error } = state;
 
-  const fetchUsers = async () => {
-    try {
-      // 요청이 시작 할 때에는 error 와 users 를 초기화하고
-      setError(null);
-      setUsers(null);
-      //loading 상태를 true 로 바꿉니다.
-      setLoading(true);
-      const response = await axios.get("https://jsonplaceholder.typicode.com/users");
-      setUsers(response.data); // 데이터는 response.data 에 들어있습니다.
-    } catch (e) {
-      setError(e);
-    }
-    setLoading(false); // 로딩완료.
-  };
-
-  useEffect(() => {
-    fetchUsers();
-  }, []);
-
-  if (loading) return <div>로딩중..</div>; // 로딩상태라면 해당 내용을
-  if (error) return <div>에러가 발생했습니다.</div>; // 에러가 발생했다면 해당 내용을
-  if (!users) return null; // API 를 불러왔는데 내용이 없다면 아무것도 렌더링하지 않습니다.
+  if (loading) return <div>로딩중..</div>;
+  if (error) return <div>에러가 발생했습니다.</div>;
+  if (!users) return <button onClick={refetch}>불러오기</button>;
 
   return (
     <>
       <ul>
         {users.map((user) => (
-          <li key={user.id}>
+          <li key={user.id} onClick={() => setUserId(user.id)} style={{ cursor: "pointer" }}>
             {user.username} ({user.name})
           </li>
         ))}
       </ul>
-      <button onClick={fetchUsers}>다시 불러오기</button>
+      <button onClick={refetch}>다시 불러오기</button>
+      {userId && <User id={userId} />}
     </>
   );
 }
